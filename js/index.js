3c1683c18a8f2b169eeaa3862b9f250bdd54b8db
Ashok Koduru <ashok.koduru9@gmail.com>
2017-09-15 01:37:09 -0400

    Changed to filterisr

--- js/index.js
+++ js/index.js
@@ -12,6 +12,36 @@ $(document).ready(function () {
         });
     });
 
+    //Default options
+    var options = {
+        animationDuration: 0.5, //in seconds
+        filter: '1', //Initial filter
+        callbacks: {
+            onFilteringStart: function() { },
+            onFilteringEnd: function() { },
+            onShufflingStart: function() { },
+            onShufflingEnd: function() { },
+            onSortingStart: function() { },
+            onSortingEnd: function() { }
+        },
+        delay: 0, //Transition delay in ms
+        delayMode: 'progressive', //'progressive' or 'alternate'
+        easing: 'ease-out',
+        filterOutCss: { //Filtering out animation
+            opacity: 0,
+            transform: 'scale(0.5)'
+        },
+        filterInCss: { //Filtering in animation
+            opacity: 0,
+            transform: 'scale(1)'
+        },
+        layout: 'sameSize', //See layouts
+        selector: '.filtr-container',
+        setupControls: true
+    };
+
+    var filterizd = $('.filtr-container').filterizr(options);
+    filterizd.filterizr('setOptions', options);
 
 
 });
